fbf02841db14c1917e9e8cdfe2756d455cc05b97
UprightBiswa <djdas000000@gmail.com>
2023-04-27 00:51:37 +0530

    ecommerce using laravel livewire category page , and brand page

---
+++ resources/views/livewire/admin/brand/index.blade.php
@@ -0,0 +1,64 @@
+
+<div>
+    @include('livewire.admin.brand.modal-form')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h4>
+                    Brands List
+                    <a href="#" data-bs-toggle="modal" data-bs-target="#addBrandModel" class="btn btn-primary btn-sm float-end">Add Brands</a>
+                </h4>
+            </div>
+            <div class="card-body">
+                <table class="table table-boardered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Slug</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($brands as $brand )
+                        <tr>
+                            <td>{{ $brand->id }}</td>
+                            <td>{{ $brand->name }}</td>
+                            <td>{{ $brand->slug }}</td>
+                            <td>{{ $brand->status == '1' ? 'hidden':'visible' }}</td>
+                            <td>
+                                <a href="" class="btn btn-success">Edit</a>
+                                <a href="" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="5">No Brands Found</td>
+                        </tr>
+
+                        @endforelse
+
+
+                    </tbody>
+
+                </table>
+                <div>
+                    {{ $brands->links() }}
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+@push('script')
+<script>
+window.addEventListener('close-modal',event=>{
+    $('#addBrandModel').modal('hide');
+});
+</script>
+
+@endpush
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Controllers\Admin\CategoryController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Livewire\Admin\Brand\Index;
+use App\Models\Category;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function ()
+ { return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function(){
+    Route::get('dashboard',[DashboardController::class,'index']);
+
+    //category routes
+    Route::controller(CategoryController::class)->group(function () {
+        Route::get('/category','index');
+        Route::get('/category/create','create');
+        Route::post('/category','store');
+        Route::get('/category/{category}/edit','edit');
+        Route::put('/category/{category}', 'update');
+    });
+
+    Route::get('/brands',Index::class);
+
+
+
+    });
+
